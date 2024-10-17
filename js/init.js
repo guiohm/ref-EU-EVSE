7a4b737f18a130e1f4aa83c28dfd35aa63561f34
guiohm <guiohm@users.noreply.github.com>
2024-10-17 22:13:53 +0000

    deploy: eff6409dfe70466c77a7d45b742a86fc80bd100a

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
-import { events } from "./constants.js";
-import { tic, toc } from "./utils.js";
+import { loadFromUrl } from "./components.js";
+import { events, state } from "./constants.js";
+import { loadLocalStorage, tic, toc } from "./utils.js";
 
 export function init(showError) {
 
@@ -22,36 +23,8 @@ export function init(showError) {
         }
     })()
 
-    // Add syntax highlihjting to the textarea
-    const editor = CodeMirror.fromTextArea(document.getElementById('input'), {
-        mode: 'text/x-sql',
-        viewportMargin: Infinity,
-        indentWithTabs: true,
-        smartIndent: true,
-        lineNumbers: false,
-        lineWrapping: true,
-        matchBrackets: true,
-        autofocus: true,
-        theme: "blackboard",
-        viewportMargin: Infinity,
-        extraKeys: {
-            "Ctrl-Enter": () =>
-                document.dispatchEvent(new Event(events.execUserSql)),
-            "Alt-Enter": "autocomplete",
-            "Alt-Space": "autocomplete",
-            "Ctrl-Space": "autocomplete",
-            "Ctrl-/": "autocomplete",
-        },
-        hint: CodeMirror.hint.sql,
-        hintOptions: {
-            tables: { //TODO automate this
-                logs: [ "level","station_id","source","msg","detail","pdc_id","nom_amenageur","siren_amenageur REAL","contact_amenageur","nom_operateur","contact_operateur","telephone_operateur","nom_enseigne","id_station_itinerance","id_station_local","nom_station","implantation_station","adresse_station","code_insee_commune","coordonneesXY","nbre_pdc","id_pdc_itinerance","id_pdc_local","puissance_nominale REAL","prise_type_ef","prise_type_2","prise_type_combo_ccs","prise_type_chademo","prise_type_autre","gratuit","paiement_acte","paiement_cb","paiement_autre","tarification","condition_acces","reservation","horaires","accessibilite_pmr","restriction_gabarit","station_deux_roues","raccordement","num_pdl","date_mise_en_service","observations","date_maj","cable_t2_attache","last_modified","datagouv_dataset_id","datagouv_resource_id","datagouv_organization_or_owner","created_at","consolidated_longitude REAL","consolidated_latitude REAL","consolidated_code_postal REAL","consolidated_commune","consolidated_is_lon_lat_correct","consolidated_is_code_insee_verified"]
-            },
-            defaultTable: "logs"
-        },
-    });
+    loadLocalStorage(state);
+    loadFromUrl(window.location.href);
 
-
-
-    return {worker, editor}
+    return {worker}
 }
--- js/main.js
+++ js/main.js
@@ -1,79 +1,44 @@
 import { init } from './init.js'
-import { events, queries, state } from './constants.js'
-import { loadLocalStorage, save2LocalStorage, tic, toc } from './utils.js';
-import { component } from '../lib/reef.es.js';
+import { events, signalNameSpaces, state, transient } from './constants.js'
+import { tic, toc } from './utils.js';
+import { createGrid, createTable, editor, grid, help } from './components.js';
 
-const {worker, editor} = init(execEditorContents, showError);
+const {worker} = init(showError);
 
-//////// Actions
-
-const ActionsEl = document.querySelector('.actions')
-
-function renderActions() {
-    ActionsEl.innerHTML = queries.map((query, index) => {
-        return `<div class="item" data-idx="${index}">${query.title}</div>`
-    }).join('')
-}
-renderActions();
-
-function onActionsClick(ev) {
-    if (!ev.target.dataset?.idx) return;
-    editor.setValue(queries[ev.target.dataset.idx].sql);
-    execEditorContents()
-}
-ActionsEl.addEventListener('click', onActionsClick, true);
-
-///////// Result count
-
-function getResultCountHtml() {
-    let {loading, resultCount} = state;
-    return loading ? 'Requête en cours...'
-        : (resultCount > 0 ? `${resultCount} résultats` : 'Pas de résultat')
-}
-component(document.getElementById('resultCount'), getResultCountHtml);
-
-//////// Toggle Result raw table / grid.js aka gridSwitch
-
-document.getElementById('result-toggle').addEventListener('click', e => {
-    setTimeout(() => {
-        state.resultUseBasicRenderer = !e.target.checked;
-        if (grid) grid.destroy()
-        save2LocalStorage(state);
-        document.dispatchEvent(new Event(events.execUserSql))
-    }, 300);
-})
-
-///////// Help modal
-
-const showHelpBtn = document.getElementById("helpShow");
-const closeBtn = document.getElementById("helpClose");
-const dialog = document.getElementById("helpDialog");
-
-showHelpBtn.addEventListener('click', () => dialog.showModal());
-closeBtn.addEventListener('click', () => {
-  dialog.close();
-});
-
-/////////
-
-let grid
 const resultsDiv = document.getElementById('results')
 const submitBtn = document.getElementById('submit')
 const errorEl = document.getElementById('error')
+const pageDiv = document.getElementsByClassName('page')[0];
 
 function showError(e) {
     loadingStop();
     console.log(e);
     errorEl.style.height = 'auto';
     errorEl.textContent = e.message;
-    state.resultCount = 0;
+    transient.resultCount = 0;
 }
+
 function cleanErrors() {
     errorEl.style.height = '0';
 }
 
+function loadingStart() {
+    transient.loading = true;
+    transient.resultCount = 0;
+    submitBtn.ariaDisabled = true;
+    pageDiv.style.opacity = 0.4;
+    if (grid) grid.destroy();
+    resultsDiv.innerHTML = '';
+}
+
+function loadingStop() {
+    transient.loading = false;
+    pageDiv.style.opacity = 1;
+    submitBtn.ariaDisabled = false;
+}
+
 function execEditorContents(options) {
-    if (state.loading) return;
+    if (transient.loading) return;
     cleanErrors()
     executeSqlAndShowResults(editor.getValue(), options);
 }
@@ -96,10 +61,10 @@ function executeSqlAndShowResults(sql, options) {
             if (state.resultUseBasicRenderer) {
                 resultsDiv.appendChild(createTable(results[i].columns, results[i].values));
             } else {
-                createGrid(results[i]);
+                createGrid(results[i], resultsDiv);
             }
         }
-        state.resultCount = results[0]?.values.length ?? 0
+        transient.resultCount = results[0]?.values.length ?? 0
         loadingStop();
         toc('Results to HTML');
     }
@@ -107,101 +72,57 @@ function executeSqlAndShowResults(sql, options) {
     loadingStart();
 }
 
-const pageDiv = document.getElementsByClassName('page')[0];
-
-function loadingStart() {
-    state.loading = true;
-    state.resultCount = 0;
-    submitBtn.ariaDisabled = true;
-    pageDiv.style.opacity = 0.4;
-    if (grid) grid.destroy();
-    resultsDiv.innerHTML = '';
-}
-
-function loadingStop() {
-    state.loading = false;
-    pageDiv.style.opacity = 1;
-    submitBtn.ariaDisabled = false;
-}
-
-let createTable = function () {
-    function valconcat(vals, tagName) {
-        if (vals.length === 0) return '';
-        const open = '<' + tagName + '>', close = '</' + tagName + '>';
-        return open + vals.join(close + open) + close;
-    }
-    return function (columns, values) {
-        const div = document.createElement('div');
-        div.classList.add('results-table');
-        let html = '<table><thead>' + valconcat(columns, 'th') + '</thead>';
-        const rows = values.map(function (v) { return valconcat(v, 'td'); });
-        html += '<tbody>' + valconcat(rows, 'tr') + '</tbody></table>';
-        div.innerHTML = html;
-        return div;
-    }
-}();
-
-function createGrid(results) {
-    if (!grid) {
-        grid = new gridjs.Grid({
-            columns: results.columns,
-            data: results.values,
-            fixedHeader: true,
-            // resizable: true,
-            search: true,
-            sort: true,
-            style:{
-                container:{
-                'width':'100%',
-                'height':'100%'
-                },
-            },
-            width: '100%',
-            height: '92%',
-        });
-    } else {
-        grid.destroy()
-    }
-    state.resultCount = results.values.length
-
-    grid.updateConfig({
-        columns: results.columns,
-        data: results.values,
-        // height: `${Math.min(900, lineCount * 190)}px`,
-    }).render(resultsDiv);
-}
-
-loadLocalStorage(state);
-document.addEventListener(events.execUserSql, execEditorContents);
+submitBtn.addEventListener('click', execEditorContents, true);
+document.addEventListener(events.execUserSql, e => execEditorContents(e.detail));
 document.addEventListener(events.dbLoaded, () => {
-    state.loading = false;
+    transient.loading = false;
     execEditorContents();
 });
-submitBtn.addEventListener('click', execEditorContents, true);
 
-//////// history
-
-function loadFromUrl(url) {
-    let sql = url.split('#')[0];
-    sql = sql.split('?q=');
-    if (sql.length > 1 && sql[1].toLowerCase().startsWith('select')) {
-        editor.setValue(decodeURI(sql[1]));
-        execEditorContents({pushHistory: false});
+document.addEventListener('keydown', e => {
+    if (e.key == '?' && !editor.hasFocus()) {
+        if (help.open) help.close(); else help.showModal();
     }
+    else if (e.ctrlKey && e.key == 'Enter') {
+        document.dispatchEvent(new Event(events.execUserSql));
+    }
+    else if (e.ctrlKey && e.key == 'z') {
+        editor.execCommand('undo');
+        editor.focus();
+    }
+    else if (e.ctrlKey && e.key == 'v') {
+        paste(e);
+    }
+});
+
+function paste(event) {
+    const selection = window.getSelection();
+    // if nothing selected -> isCollapsed == true -> the user clipboard will be pasted
+    // if something selected -> we paste the selection but NOT the user clipboard
+    if (!selection.isCollapsed) {
+        // check for template placeholders and fill the first one
+        let sql = editor.getValue();
+        const regex = /(_value_)/;
+        let found = false;
+        sql = sql.replace(regex, match => {
+            found = match != null;
+            return selection.toString();
+        });
+        if (found) {
+            editor.setValue(sql);
+        } else {
+            editor.replaceSelection(selection.toString());
+        }
+        event.preventDefault();
+    }
+    editor.focus();
 }
-
-window.addEventListener('popstate', (e) => loadFromUrl(e.target.location.href));
-
-// initial load
-loadFromUrl(window.location.href);
-
-////////
 
 // select cell content on click
 resultsDiv.addEventListener('click', e => {
     if (e.target.nodeName == 'TD') {
         const selection = window.getSelection();
-        selection.removeAllRanges();
+        selection.empty();
         const range = document.createRange();
         range.selectNodeContents(e.target);
         selection.addRange(range);
@@ -209,5 +130,7 @@ resultsDiv.addEventListener('click', e => {
 })
 
 // debug
-// for (let e of ['signal', 'start', 'stop', 'before-render', 'render'])
+// for (let e of ['start', 'stop', 'before-render', 'render'])
 //     document.addEventListener('reef:' + e, ev => console.log(ev.type, ev.target));
+// for (let n of Object.keys(signalNameSpaces))
+//     document.addEventListener('reef:signal-' + n, ev => console.log('signal-'+n, ev.detail));
